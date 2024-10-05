626dccad4759fc57bad95edce8d9887e3f4a6459
Evan Marie Carr <evanmariecarr@gmail.com>
2024-10-05 12:05:25 -0400

    divider heading

---
+++ app/buildingBlockComponents/dividerHeading.tsx
@@ -0,0 +1,68 @@
+import { FlexFull, HStackFull } from "./mainContainers";
+import Text from "./text";
+
+export default function DividerHeading({
+  heading,
+  width,
+  dividerBg = "bg-col-500",
+  dividerHeight = "h-0.1vh",
+  padding = "py-1vh px-2vh",
+  textSize = "text-xl",
+  textColor = "text-col-500",
+  textShadow = "textShadow",
+  dividerShadow = "shadowNarrowNormal",
+  gap = "gap-1.5vh",
+  fontFamily,
+  bothSides = true,
+  rightSide,
+  leftSide,
+  className = "",
+}: {
+  heading: string;
+  width?: string;
+  dividerBg?: string;
+  dividerHeight?: string;
+  fontFamily?: string;
+  padding?: string;
+  textSize?: string;
+  textColor?: string;
+  textShadow?: string;
+  dividerShadow?: string;
+  gap?: string;
+  bothSides?: boolean;
+  rightSide?: boolean;
+  leftSide?: boolean;
+  className?: string;
+}) {
+  function Divider({
+    height = dividerHeight,
+    bg = dividerBg,
+    shadow = dividerShadow,
+  }: {
+    height?: string;
+    bg?: string;
+    shadow?: string;
+  }) {
+    return (
+      <FlexFull className={`text-transparent ${height} ${bg} ${shadow}`}>
+        .
+      </FlexFull>
+    );
+  }
+
+  return (
+    <HStackFull
+      className={`${width} ${className} ${padding} ${gap} items-center`}
+    >
+      {bothSides && !rightSide && !leftSide && <Divider />}
+      {leftSide ? <Divider /> : null}
+      <span
+        className={`${textSize} ${textColor} ${textShadow} ${fontFamily} flex flex-shrink-0 text-nowrap`}
+      >
+        {heading}
+      </span>
+      {rightSide && <Divider />}
+      {bothSides && !leftSide && !rightSide && <Divider />}
+    </HStackFull>
+  );
+}
